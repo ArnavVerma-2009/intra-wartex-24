e560e18a2c2a82dad7029f5d9219c7f4c2233af7
ARNAVVERMAbang <arnav.verma.feb.2009@gmail.com>
2024-04-09 23:30:51 +0530

    remove lenis

--- src/App.jsx
+++ src/App.jsx
@@ -45,7 +45,6 @@ import transition from "./transition";
 import Video from "./components/Video";
 
 
-import Lenis from "@studio-freight/lenis";
 
 
 // Scroll to top function
@@ -59,25 +58,6 @@ function scrollToTop() {
 }
 
 function App() {
-
-  const lenis = new Lenis({
-    duration: 1, // speed
-    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)), // https://www.desmos.com/calculator/brs54l4xou
-    direction: "vertical", // vertical, horizontal
-    gestureDirection: "vertical", // vertical, horizontal, both
-    smooth: true,
-    mouseMultiplier: 0.5, // sensibility
-    smoothTouch: false, // Mobile
-    touchMultiplier: 2, // sensibility on mobile
-    infinite: false, // Infinite scrolling
-  });
-
-  function raf(time) {
-    lenis.raf(time);
-    requestAnimationFrame(raf);
-  }
-
-  requestAnimationFrame(raf);
   return (
     <>
       <AnimatePresence  mode="wait">
--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ export default defineConfig({
   base : './',
   server: {
     host: true
-  }
+  },
 })
